f895526faa630b5159027482325198911ed049f1
DaneThomas <musicisamiracle@yahoo.com>
2016-07-15 22:13:16 -0500

    Nothing important

--- JackCompiler.py
+++ JackCompiler.py
@@ -1,6 +1,8 @@
 from lxml import etree as ET
 from CompilationEngine import CompilationEngine
 
+"""Need to change access to class variables into class methods"""
+
 try:
     c = CompilationEngine('Seven/Main.jack')
     c.compile_class()
